31c77ecc919d11fdc3ba173bf4067e99f94c5c95
Your Name <roberttoribio04@gmail.com>
2025-05-30 17:52:51 -0700

    adds brute force solution

--- 2017p1/cow_crossing.py
+++ 2017p1/cow_crossing.py
@@ -1,6 +1,7 @@
 def read_input(file):
     '''
-        returns the nums list and the range of queries
+        returns the chicken times in sorted order, and the time range the
+        chickens are available
     '''
     lines = [line.strip('\n') for line in open(file,'r')]
     C,N = lines[0].split()
@@ -12,12 +13,29 @@ def read_input(file):
     for j in range(C+1,len(lines)):
         line = lines[j].split()
         AB.append((int(line[0]),int(line[1])))
+    return sorted(T),AB
+def solve(T,AB):
+    '''
+        Brute Force: loop thru T and check if they 
 
-    print(T)
-    print(AB)
+        returns: the maximum # of cow-chicken pairs that can be constructed
+    '''
+    count = 0
+    used = set()
+    for t in T:
+        for l,u in AB:
+            # print(t)
+            # print(l,u)
+            if t in range(l,u+1) and (l,u) not in used:
+                used.add((l,u))
+                count +=1
+                break
 
+    return count
 def main():
     print("from main..")
-    read_input('2017p1\helpcross.in')
+    T,AB =read_input('2017p1\helpcross.in')
+    print(solve(T,AB))
+    
 if __name__=="__main__":
     main()
